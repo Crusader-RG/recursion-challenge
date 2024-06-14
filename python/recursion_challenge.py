c964b73e86cbad29742d691eb55a1b14ebd33798
Crusader-RG <justin.leeper92@gmail.com>
2024-06-14 16:34:23 -0600

    finished python

--- python/recursion_challenge.py
+++ python/recursion_challenge.py
@@ -1,11 +1,45 @@
 def factorial(x):
-	pass
+	if x == 1:
+		return x
+	return x * factorial(x - 1)
+		
 
 def palindrome(string):
-	pass
+	if len(string) < 1:
+		return True
+	if string[0] == string[-1]:
+		return palindrome(string[1:-1])
+	else:
+		return False
 
 def bottles(num):
-	pass
+	if num == 1:
+		print(f"1 bottle\nno bottles")
+		return
+	print(f"{num} bottles")
+	return bottles(num - 1)
 
 def roman_num(num):
-	pass
+	roman = {
+        "M": 1000,
+        "CM": 900,
+        "D": 500,
+        "CD": 400,
+        "C": 100,
+        "XC": 90,
+        "L": 50,
+        "XL": 40,
+        "X": 10,
+        "IX": 9,
+        "V": 5,
+        "IV": 4,
+        "I": 1 
+     }
+	
+	if num == 0:
+		return ""
+	for key in roman.keys():
+		if roman[key] <= num:
+			return f"{key}" + f"{roman_num(num - roman[key])}"
+		
+
--- python/recursion_spec.py
+++ python/recursion_spec.py
@@ -1,1 +1,31 @@
-# Write your unit tests here
+import pytest
+from recursion_challenge import factorial
+from recursion_challenge import palindrome
+from recursion_challenge import bottles
+from recursion_challenge import roman_num
+
+def test_recursion_challenge():
+    assert factorial(5) == 120
+    assert factorial(3) == 6
+    assert factorial(13) == 6227020800
+
+@pytest.mark.parametrize("num, expected_output", [
+    (3, "3 bottles\n2 bottles\n1 bottle\nno bottles\n"),
+    (4, "4 bottles\n3 bottles\n2 bottles\n1 bottle\nno bottles\n")
+])
+def test_bottles(capfd, num, expected_output):
+    bottles(num)
+    captured = capfd.readouterr()
+    assert captured.out == expected_output
+
+def test_palindrome():
+    assert palindrome("racecar") == True
+    assert palindrome('justin') == False
+    assert palindrome('pv1vp') == True
+    assert palindrome('1234321') == True
+
+def test_roman_num():
+    assert roman_num(3) == "III"
+    assert roman_num(10) == "X"
+    assert roman_num(27) == "XXVII"
+    assert roman_num(49) == "XLIX"
